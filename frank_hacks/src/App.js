062c08af2978cc4f4e483877a9927d09ea0a0a79
Wills Stern <willsstern@gmail.com>
2022-10-16 03:16:04 -0700

    Setting up CI/CD for site

--- frank_hacks/src/App.js
+++ frank_hacks/src/App.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
+//import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import SurfingIcon from '@mui/icons-material/Surfing';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Search from './search';
 
 const theme = createTheme();
 
@@ -21,26 +21,6 @@ export default function Locate() {
     });
   };
 
-  beaches = ['Kaimu Black Sand Beach, HI',
-  'Glass Beach, CA',
-  'Siesta Key Beach, FL',
-  'Marshall\'s Beach, CA',
-  'Honokalani Beach, HI',
-  'Mauna Kea Beach, HI',
-  'North Myrtle Beach, SC',
-  'Edisto Island Beach, SC',
-  'Cedar Beach, NY',
-  'Sebastian Beach Inn, FL',
-  'Potomac Standing Wave, MD',
-  'Ortley Beach, Toms River, NJ',
-  'Trump International Beach Resort, FL',
-  'Sunset Beach, FL',
-  'Chapman Point, Cannon Beach, OR',
-  'Haulover Beach, FL',
-  'Beach & Boardwalk, Seaside Park, NJ',
-  'Ocean Isle Beach, NC',
-  'Mutiny Bay, WA']
-
   return (
     <ThemeProvider theme={theme}>
       <Grid container component="main" sx={{ height: '100vh' }}>
@@ -51,7 +31,7 @@ export default function Locate() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://wallpaperaccess.com/full/1540066.jpg)',
+            backgroundImage: 'url(https://preview.redd.it/so6zsvih25u91.jpg?width=960&crop=smart&auto=webp&s=49b36ef972f741b9cb09e58687c35a6cba00e3bf)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
@@ -62,38 +42,21 @@ export default function Locate() {
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
-              my: 22,
+              my: 25,
               mx: 4,
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main', backgroundImage: 'url(https://i.redd.it/1ka2cmdk35u91.jpg)' }}>
               <SurfingIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
               It's Time to Shred
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="text"
-                label="Find Your Beach"
-                name="text"
-                autoComplete="text"
-                autoFocus
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Locate
-              </Button>
+              <Search />
               
             </Box>
           </Box>
