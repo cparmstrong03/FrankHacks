4bebd0d9842548cdb27bee4483f8a45c15d07872
Flynn Gray <flynngray@Flynns-MacBook-Pro.local>
2022-10-15 19:01:11 -0700

    minority report

---
+++ CrimeMaps.py
@@ -0,0 +1,5 @@
+import requests
+
+r = requests.get('https://api.usa.gov/crime/fbi/sapi/api /data/supplemental/not-specified/states/CA/OFFENSE/2000/2022?API_KEY=NA9DrPF8yhGVtuH88SdGPhU2jn5pG59TulaZVxjR')
+
+print(r)
