2cc4c7dc8f4b16ddb89204b6ae13f157e922452e
Connor Armstrong <cparmstrong5@berkeley.edu>
2022-10-16 01:39:54 -0700

    panda

--- wimbdy.py
+++ wimbdy.py
@@ -1,5 +1,5 @@
 import requests
-import json
+import csv
 
 LAT = "25.77481"
 LONG = "-81.19773"
@@ -8,26 +8,26 @@ combo = LAT + "," + LONG
 
 url = "https://visual-crossing-weather.p.rapidapi.com/forecast"
 
-querystring = {"aggregateHours":"24","location":combo,"contentType":"json","unitGroup":"us","shortColumnNames":"1"}
+querystring = {"aggregateHours":"24","location":combo,"contentType":"csv","unitGroup":"us","shortColumnNames":"1"}
 
 headers = {
 	"X-RapidAPI-Key": "6520434f99mshb47aa5f323c8a54p16f385jsn176f39a1c3c6",
 	"X-RapidAPI-Host": "visual-crossing-weather.p.rapidapi.com"
 }
 
-#testStr = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/38.9697,-77.385?key=6520434f99mshb47aa5f323c8a54p16f385jsn176f39a1c3c6"
 
 response = requests.request("GET", url, headers=headers, params=querystring)
-#response = requests.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/38.9697,-77.385?key=6520434f99mshb47aa5f323c8a54p16f385jsn176f39a1c3c6")
 
 print(response)
 print(type(response))
 print(response.apparent_encoding)
-jRes = response.json()
-
-print(jRes.keys())
 
 
+decoded_content = response.content.decode('utf-8')
 
+cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+my_list = list(cr)
+for row in my_list:
+    print(row)
 
 
