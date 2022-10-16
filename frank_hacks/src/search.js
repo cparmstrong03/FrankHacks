e444bdb69fbdcaed5bb91a7d1a0d20aac5ba058a
Wills Stern <willsstern@gmail.com>
2022-10-16 06:26:26 -0700

    Web update

--- frank_hacks/src/search.js
+++ frank_hacks/src/search.js
@@ -1,15 +1,58 @@
 /* eslint-disable no-use-before-define */
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from 'react-dom/client';
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 
 export default function Search() {
+
+  const [helpText, setHelpText] = useState(" ");
+
   function handleInputChange(event, value) {
     for (let i = 0; i < beaches.length; i++) {
       if (value === beaches[i])
-        JSON.stringify(value);
+        setHelpText(value);
     }
   }
+
+  // const root2 = ReactDOM.createRoot(document.getElementById('root2'));
+
+  // if (helpText.length > 2) {
+  //   console.log("Worked!");
+  //   document.write(helpText);
+  //   root2.render(
+  //     <py-script>
+
+  //       beaches = [
+  //         'Kaimu Black Sand Beach, HI',
+  //         'Glass Beach, CA',
+  //         'Siesta Key Beach, FL',
+  //         'Marshall\'s Beach, CA',
+  //         'Honokalani Beach, HI',
+  //         'Mauna Kea Beach, HI',
+  //         'North Myrtle Beach, SC',
+  //         'Edisto Island Beach, SC',
+  //         'Cedar Beach, NY',
+  //         'Sebastian Beach Inn, FL',
+  //         'Potomac Standing Wave, MD',
+  //         'Ortley Beach, Toms River, NJ',
+  //         'Trump International Beach Resort, FL',
+  //         'Sunset Beach, FL',
+  //         'Chapman Point, Cannon Beach, OR',
+  //         'Haulover Beach, FL',
+  //         'Beach & Boardwalk, Seaside Park, NJ',
+  //         'Ocean Isle Beach, NC',
+  //         'Mutiny Bay, WA'
+  //       ]
+
+  //       element = document.getElementById("combo-box-demo-helper-text")
+        
+  //       val = element.innerText
+  //       print(val)
+
+  //     </py-script>
+  //   );
+  // }
 
   return (
     <Autocomplete
@@ -19,7 +62,7 @@ export default function Search() {
       style={{ width: 300 }}
       onInputChange={handleInputChange}
       renderInput={params => (
-        <TextField {...params} label="Find Your Beach" variant="outlined" fullWidth />
+        <TextField {...params} label="Find Your Beach" helperText = {helpText} value variant="outlined" fullWidth />
       )}
     />
   );
