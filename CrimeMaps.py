723de7304252fea5875c707ccbbb9cb08726028a
Flynn Gray <flynngray@Flynns-MacBook-Pro.local>
2022-10-15 19:07:40 -0700

    flynn push test

--- CrimeMaps.py
+++ CrimeMaps.py
@@ -1,5 +1,7 @@
 import requests
 
-r = requests.get('https://api.usa.gov/crime/fbi/sapi/api /data/supplemental/not-specified/states/CA/OFFENSE/2000/2022?API_KEY=NA9DrPF8yhGVtuH88SdGPhU2jn5pG59TulaZVxjR')
+r = requests.get('https://api.usa.gov/crime/fbi/sapi/api/data/supplemental/not-specified/states/CA/OFFENSE/2000/2022?API_KEY=NA9DrPF8yhGVtuH88SdGPhU2jn5pG59TulaZVxjR')
 
 print(r)
+
+#change
